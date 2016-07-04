59c835ab6d0a10eceeec2c63a401a8ab5af1668b
weiy <503945930@qq.com>
2016-07-04 19:30:24 +0800

    base strcture

--- project/spider/gjspider.py
+++ project/spider/gjspider.py
@@ -1,7 +1,15 @@
+# coding=utf-8
+
 import requests
-
-
-
+from bs4 import BeautifulSoup
+import re
 def gjscrapy():
-    url = ""
-    print  requests.post(url)
+    url = "http://chengdu.anjuke.com/sale/p2-rd1"
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+    result = requests.get(url,params={'from':'zjsr','kw':'东立国际花城'},headers = headers)
+    soup = BeautifulSoup(result.text,"lxml")
+    print soup.find_all("li",class_=re.compile("list"))
+
+
+if __name__ == '__main__':
+    gjscrapy();
